33cd546fb7dbfd4671020a0585c166e325a46de6
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-06-21 03:11:45 -0400

    everything is working from video

---
+++ src/graph.py
@@ -0,0 +1,30 @@
+#make a class for edges. Use capital letters. Init is a built in function
+class Edge:
+    def __init__(self, destination):
+        self.destination = destination
+#** keyword arguments. lets us create arg with keywords. Uses square brackes line 19
+class Vertex:
+    def __init__(self, value, **pos): #TODO test default arguments
+        self.value = value 
+        self.color = 'white'
+        self.pos = pos
+        self.edges = [] 
+
+class Graph:
+    def __init__(self):
+        self.vertexes = []
+
+    def debug_create_test_data(self):
+        debug_vertex_1 = Vertex('t1', x=40, y=40)
+        debug_vertex_2 = Vertex('t2', x=140, y=140)
+        debug_vertex_3 = Vertex('t3', x=300, y=400)
+
+        debug_edge_1 = Edge(debug_vertex_2)
+        debug_vertex_1.edges.append(debug_edge_1)
+       
+        debug_edge_2 =Edge(debug_vertex_2)
+        debug_vertex_3.edges.append(debug_edge_2)
+
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+        
+        
