6b36dce211aa337ea44129c53675d0f8a3b0523e
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-06-21 16:16:25 -0400

    Error draw line 47

--- src/draw.py
+++ src/draw.py
@@ -7,10 +7,14 @@ from bokeh.palettes import Spectral8
 
 from graph import * 
 
+WIDTH = 500
+# HEIGHT 500 #TODO currently graph renders sq Possible fix by Ajmal in stackoverflow
+CIRCLE_SIZE = 30
+
 graph_data = Graph()
 graph_data.debug_create_test_data()
-print(graph_data.vertexes)
-
+#print(graph_data.vertexes)
+graph_data.bfs(graph_data.vertexes[0])
 
 N = len(graph_data.vertexes) # N=8 makes 8 offshoots from one point. Theese are vertexes or nodes
 node_indices = list(range(N))
@@ -19,20 +23,30 @@ color_list = [] #random colors
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Graph Layout Demonstration', x_range=(0, WIDTH), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color') # makes ovals
+graph.node_renderer.glyph = circle(size=CIRCLE_SIZE, fill_color='color') # makes ovals
+
 
 #TODO this is drawing the edges from start to end
+start_indexes = []
+end_indexes = []
+
+for start_index, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indexes.append(start_index)
+        end_indexes.append(graph_data.vertexes.index(e.destination))
+
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N, # this is a list of some kind that has to do with starting points
-    end=node_indices) # this is a list of some kind that has to do with ending points
-    
+    start=start_indexes # this is a list of some kind that has to do with starting points
+    end= end_indexes # this is a list of some kind that has to do with ending points
+)
+ print(graph.edge_renderer.data_source.data)   
 ### start of layout code
 # looks like it sets positions of vertexes
 
@@ -43,6 +57,23 @@ graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+
+
+#Create a new dict to use a data source with 3 lists in it ordered the same wayas vertexes
+# List of x values
+# List of y values
+# List of labels
+value =[v.value for v in graph_data.vertexes] #TODO possible optimization we run through loop 3 times
+
+label_source = ColumnDataSource(data=dict(x=x, y=y, value))
+
+
+labels = LabelSet(x='x', y='y', text='v', level='glyph',
+            source=label_source, render_mode='canvas', text_align='center', text_baseline='middle')
+
+#TODA Investigate plot.add_layout vs. plot.reducers.append
+
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,5 @@
+import random
+
 #make a class for edges. Use capital letters. Init is a built in function
 class Edge:
     def __init__(self, destination):
@@ -18,6 +20,7 @@ class Graph:
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=140, y=140)
         debug_vertex_3 = Vertex('t3', x=300, y=400)
+        debug_vertex_3 = Vertex('t3', x=300, y=400)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
@@ -26,5 +29,27 @@ class Graph:
         debug_vertex_3.edges.append(debug_edge_2)
 
         self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+
+    def bfs(self, start):
+        random_color = [random.randint(0, 255)random.randint(0, 255)random.randint(0, 255)]
+        #  "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+        queue = []
+        found = []
+        
+        queue.append(start)
+        found.append(start)
+
+        start.color = random_color
+
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0) #TODO look into collections.dequeue
+        
+       # return found
         
         
