f617951855817363bcf488d46478b5741807f6c7
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-06-22 11:19:52 -0400

    working during standup

--- src/graph.py
+++ src/graph.py
@@ -20,7 +20,7 @@ class Graph:
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=140, y=140)
         debug_vertex_3 = Vertex('t3', x=300, y=400)
-        debug_vertex_3 = Vertex('t3', x=300, y=400)
+        debug_vertex_4 = Vertex('t4', x=400, y=500)
 
         debug_edge_1 = Edge(debug_vertex_2)
         debug_vertex_1.edges.append(debug_edge_1)
@@ -28,11 +28,10 @@ class Graph:
         debug_edge_2 =Edge(debug_vertex_2)
         debug_vertex_3.edges.append(debug_edge_2)
 
-        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4])
 
     def bfs(self, start):
-        random_color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-        "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+        random_color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
         queue = []
         found = []
 
